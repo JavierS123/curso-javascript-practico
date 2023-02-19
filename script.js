0cf6facb364bb414574e2a0216ae55e6e6c12d9f
Javier Sanchez <jesg60@gmail.com>
2023-02-18 20:52:22 -0400

    HTML a partir de Arrays

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const mobileMenuIcon = $('.mobile-menu-icon')
 const mobileMenu = $('.mobile-menu');
 const menuCarritoIcon = $('.navbar-shopping-cart');
 const aside = $('.product-detail')
+const cardsContainer = $('.cards-container')
 
 menuEmail.addEventListener('click', showMyDesktopMenu);
 
@@ -26,4 +27,54 @@ function showMyOrder(){
     aside.classList.toggle('inactive');
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
+}
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+productList.push({
+    name: 'Qtie',
+    price: 220,
+    image: './img/qtgirl.png'
+})
+productList.push({
+    name: 'Tokyo',
+    price: 620,
+    image: './img/Tokyo-Shibuya-neon-lights.jpg'
+})
+for (product of productList){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    //product = {name, price, image}--> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', 'product.image');
+
+    const productInfo = document.createElement('div')
+    productInfo.classList.add('product-info');
+    
+    const productInfoDiv= document.createElement('div')
+    
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+    
+    productInfoDiv.append(productPrice, productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.append(productInfoDiv, productInfoFigure)
+
+    productCard.append(productImg, productInfo)
+    
+    cardsContainer.append(productCard);
 }
